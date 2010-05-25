f708e72044231d4b3ee3b04da02448945da93b64
Rogério Brito <rbrito@ime.usp.br>
2010-05-25 06:29:16 -0300

    Initial import of xpdf-poppler

---
+++ Makefile
@@ -0,0 +1,13 @@
+LIBS=$(shell pkg-config --libs poppler) -lXm
+POPPLERBASE=$(strip $(shell pkg-config --cflags poppler))
+INCLUDES=$(POPPLERBASE) $(POPPLERBASE)/goo $(POPPLERBASE)/splash
+
+CPPFLAGS+= $(INCLUDES)
+
+xpdf: CoreOutputDev.o PDFCore.o XPDFApp.o XPDFCore.o XPDFTree.o XPDFViewer.o parseargs.o xpdf.o 
+	$(CXX) -o xpdf $(LIBS) *.o
+
+clean:
+	rm -f *.o xpdf
+
+.PHONY: clean
---
+++ XPDFApp.h
@@ -0,0 +1,108 @@
+//========================================================================
+//
+// XPDFApp.h
+//
+// Copyright 2002-2003 Glyph & Cog, LLC
+//
+//========================================================================
+
+#ifndef XPDFAPP_H
+#define XPDFAPP_H
+
+#ifdef USE_GCC_PRAGMAS
+#pragma interface
+#endif
+
+#define Object XtObject
+#include <Xm/XmAll.h>
+#undef Object
+#include "gtypes.h"
+#include "SplashTypes.h"
+
+class GooString;
+class GooList;
+class XPDFViewer;
+
+//------------------------------------------------------------------------
+
+#define xpdfAppName "Xpdf"
+
+//------------------------------------------------------------------------
+// XPDFApp
+//------------------------------------------------------------------------
+
+class XPDFApp {
+public:
+
+  XPDFApp(int *argc, char *argv[]);
+  ~XPDFApp();
+
+  XPDFViewer *open(GooString *fileName, int page = 1,
+		   GooString *ownerPassword = NULL,
+		   GooString *userPassword = NULL);
+  XPDFViewer *openAtDest(GooString *fileName, GooString *dest,
+			 GooString *ownerPassword = NULL,
+			 GooString *userPassword = NULL);
+  void close(XPDFViewer *viewer, GBool closeLast);
+  void quit();
+
+  void run();
+
+  //----- remote server
+  void setRemoteName(char *remoteName);
+  GBool remoteServerRunning();
+  void remoteOpen(GooString *fileName, int page, GBool raise);
+  void remoteOpenAtDest(GooString *fileName, GooString *dest, GBool raise);
+  void remoteReload(GBool raise);
+  void remoteRaise();
+  void remoteQuit();
+
+  //----- resource/option values
+  GooString *getGeometry() { return geometry; }
+  GooString *getTitle() { return title; }
+  GBool getInstallCmap() { return installCmap; }
+  int getRGBCubeSize() { return rgbCubeSize; }
+  GBool getReverseVideo() { return reverseVideo; }
+  SplashColorPtr getPaperRGB() { return paperRGB; }
+  Gulong getPaperPixel() { return paperPixel; }
+  Gulong getMattePixel() { return mattePixel; }
+  GooString *getInitialZoom() { return initialZoom; }
+  GBool getViKeys() { return viKeys; }
+  void setFullScreen(GBool fullScreenA) { fullScreen = fullScreenA; }
+  GBool getFullScreen() { return fullScreen; }
+
+  XtAppContext getAppContext() { return appContext; }
+  Widget getAppShell() { return appShell; }
+
+private:
+
+  void getResources();
+  static void remoteMsgCbk(Widget widget, XtPointer ptr,
+			   XEvent *event, Boolean *cont);
+
+  Display *display;
+  int screenNum;
+  XtAppContext appContext;
+  Widget appShell;
+  GooList *viewers;		// [XPDFViewer]
+
+  Atom remoteAtom;
+  Window remoteXWin;
+  XPDFViewer *remoteViewer;
+  Widget remoteWin;
+
+  //----- resource/option values
+  GooString *geometry;
+  GooString *title;
+  GBool installCmap;
+  int rgbCubeSize;
+  GBool reverseVideo;
+  SplashColor paperRGB;
+  Gulong paperPixel;
+  Gulong mattePixel;
+  GooString *initialZoom;
+  GBool viKeys;
+  GBool fullScreen;
+};
+
+#endif
